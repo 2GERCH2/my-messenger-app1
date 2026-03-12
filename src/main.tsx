65169c1c83cfe976d5392a94785f1aa84990a252
2GERCH2 <fedredsad@gmail.com>
2026-03-12 17:17:15 +0300

    ws override

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// ✅ Переопределяем глобальный конструктор WebSocket. Любые попытки создать соединение
+// ✅ откуда угодно, из любого места кода, любым способом всегда будут идти на наш сервер.
+const OriginalWebSocket = window.WebSocket;
+window.WebSocket = function() {
+  console.log("✅ Перехвачена попытка создания вебсокет соединения, перенаправляем на наш сервер");
+  return new OriginalWebSocket('wss://botelln-server.onrender.com');
+} as any;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
